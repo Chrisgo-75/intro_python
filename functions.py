c1505bd6733c26ff40adb4de8a8deafad20f542e
Chris Arndt <christopher.arndt@wisc.edu>
2016-07-22 08:50:31 -0500

    added more function examples.

--- functions.py
+++ functions.py
@@ -5,8 +5,33 @@
 def func1():
   print('I am a function');
 
+# 1. simply calls func1
+#func1();
+
+# 2. Call and Prints Function1
+#print(func1());
+# => None  so above cmd is trying to print the return value but the function doesn't return anything.
+
+# 3. Functions in Python are Objects, they have a value.
+#    a. This is how Python treats objects.
+#print(func1);
+# => <function func1 at 0x7f001cca2bf8>
+
+# -------
+
+# function that takes parameters
+def func2(param1, param2):
+  print(param1, " ", param2);
+
+# 4. function that takes parameters
+#func2(10,20);
+#print(func2(10,20));
+
+# -------
+
+# define a function that returns a value
+# LEFT OFF 4:49
 
 
 
 
-func1(); # simply calls func1
